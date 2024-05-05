e439874faf24be1e8f516e5f71692e5a432ede34
DTP1909 <thanhtung280203@gmail.com>
2024-05-06 00:54:26 +0700

    code php

---
+++ bt_login/demo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=>h1fdv, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo"xin chào Tùng";
+        ?>
+    </h1>
+</body>
+</html>
